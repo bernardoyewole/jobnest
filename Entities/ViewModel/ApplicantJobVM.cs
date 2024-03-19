6d72681ac799a4ecd4ffd586d72e025073a090d8
Bernard <oyebenard@gmail.com>
2024-03-18 21:46:49 -0500

    Applicants with Applied Jobs View Complete

---
+++ Entities/ViewModel/ApplicantJobVM.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.ViewModel
+{
+    public class ApplicantJobVM
+    {
+        public string ApplicantName { get; set; }
+        public List<Application> AppliedJobs { get; set; }
+    }
+}
